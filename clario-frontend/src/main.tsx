af60ad44f50583d6b066ff249a729b219f481aca
Poojayadav0909 <shahsudha259@gmail.com>
2026-06-21 09:00:42 +0545

    feat: add service worker for PWA desktop install support

--- clario-frontend/src/main.tsx
+++ clario-frontend/src/main.tsx
@@ -4,3 +4,9 @@ import "./i18n";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js");
+  });
+}
